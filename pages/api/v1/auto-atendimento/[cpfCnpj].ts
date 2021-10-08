2f45d15470b082e9cf2378b42d8dc9c5dce78314
SeuJota <jalvesteixeira@gmail.com>
2021-10-08 17:15:08 -0300

    Autoatendimento endpoint

---
+++ pages/api/v1/auto-atendimento/[cpfCnpj].ts
@@ -0,0 +1,61 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+type Notification = {
+    code: number
+    message: string
+}
+
+type PossuiAcesso = {
+    possuiAcesso: boolean
+}
+
+type Response200 = {
+    data: PossuiAcesso
+    notifications?: Notification[]
+}
+
+type ResponseError = {
+    type: string
+    title: string
+}
+
+
+export default function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<Response200 | ResponseError>
+) {
+    const { cpfCnpj } = req.query
+
+    if(cpfCnpj === "23536251935") {
+        const possuiAcesso: PossuiAcesso = {
+            possuiAcesso: true
+        }
+
+        res.status(200).json({ data: possuiAcesso })
+    }
+    else if(cpfCnpj === "23536251934") {
+        const possuiAcesso: PossuiAcesso = {
+            possuiAcesso: false
+        }
+
+        res.status(200).json({ data: possuiAcesso })
+    }
+    else if(cpfCnpj === "23536251933") {
+        const error: ResponseError = {
+            title: "Usuário não cadastrado",
+            type: "Error"
+        }
+
+        res.status(400).json(error)
+    }
+    else {
+        const error: ResponseError = {
+            title: "Erro generico",
+            type: "Error"
+        }
+
+        res.status(500).json(error)
+    }
+
+}
---
+++ pages/api/v1/auto-atendimento/index.ts
@@ -0,0 +1,44 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { v4 as uuidv4 } from 'uuid';
+
+type Notification = {
+    code: number
+    message: string
+}
+
+type Guid = {
+    guid: string
+}
+
+type Response200 = {
+    data: Guid
+    notifications?: Notification[]
+}
+
+type ResponseError = {
+    type: string
+    title: string
+}
+
+export default function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<Response200 | ResponseError>
+) {
+
+    const { cpfCnpj } = req.body
+
+    if(cpfCnpj === "23536251935") {
+        const guid = uuidv4()
+        res.status(200).json({data: guid})
+    } else{
+        const error: ResponseError = {
+            title: "Erro generico",
+            type: "Error"
+        }
+
+        res.status(400).json(error)
+    }
+
+
+}
