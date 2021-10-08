73353298a46b8806e8d497ec72ed8e63b72bdbfe
SeuJota <jalvesteixeira@gmail.com>
2021-10-08 10:57:46 -0300

    Comissionado mocks and Example of dynamic routes

---
+++ pages/api/v1/comissionados/index.ts
@@ -0,0 +1,74 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { v4 as uuidv4 } from 'uuid';
+
+type Comissionado = {
+    idComissionado: number
+    codigoComissionado: string
+    nomeComissionado: string
+}
+
+type Notification = {
+    code: number
+    message: string
+}
+
+type ResponseGet = {
+    total: number
+    totalPages: number
+    page: number
+    itemsPerPage: number
+    data: Comissionado[]
+    notifications?: Notification[]
+}
+
+type IdComissionado = {
+    idComissionado: number
+}
+
+type ResponsePost = {
+    data: IdComissionado
+    notifications?: Notification[]
+}
+
+export default function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<ResponseGet | ResponsePost>
+) {
+
+    if (req.method === 'GET') {
+        const com1: Comissionado = {
+            idComissionado: 1,
+            codigoComissionado: "ABC",
+            nomeComissionado: "Pedro da comissão"
+        };
+
+        const com2: Comissionado = {
+            idComissionado: 2,
+            codigoComissionado: "DEF",
+            nomeComissionado: "Carreta furacão"
+        };
+
+        const response: ResponseGet = {
+            total: 2,
+            totalPages: 1,
+            page: 1,
+            itemsPerPage: 10,
+            data: [
+                com1,
+                com2
+            ]
+        }
+
+        res.status(200).json(response)
+    }
+
+    if (req.method === 'POST') {
+        res.status(200).json({
+            data: {
+                idComissionado: 1
+            }
+        })
+    }
+
+}
